3f098b7cb79dbcfcbdeb8e1023e085b57dc1f3e5
bratanovv <bratanov.misha@mail.ru>
2023-05-09 20:35:48 +0300

    connecting to db

--- server/controllers/vinylRecordController.js
+++ server/controllers/vinylRecordController.js
@@ -1,13 +1,32 @@
-class VinylRecordController{
-    async create(req,res){
+const {VinylRecord} = require('../models/models')
+const ApiError = require('../error/ApiError')
+const uuid = require('uuid')
+const path = require('path')
+
+
+class VinylRecordController {
+    async create(req, res, next) {
+        try {
+            const {name, price, brandId, typeId, info} = req.body
+            const {img} = req.files
+            let fileName = uuid.v4() + ".jpeg"
+            img.mv(path.resolve(__dirname, '..', 'static', fileName))
+
+            const item = await VinylRecord.create({name, price, brandId, typeId, img: fileName})
+            return res.json(item)
+        } catch (e) {
+            next(ApiError.badReqest(e.message))
+        }
+    }
+
+    async getAll(req, res) {
 
     }
-    async getAll(req,res){
 
-    }
-    async getById(req,res){
+    async getById(req, res) {
 
     }
 
 }
+
 module.exports = new VinylRecordController()
--- server/routes/vinylRecordRouter.js
+++ server/routes/vinylRecordRouter.js
@@ -2,8 +2,8 @@ const Router = require('express')
 const router = new Router()
 const vinylRecordsController = require('../controllers/vinylRecordController')
 
-router.post('/',vinylRecordsController.create)
-router.get('/',vinylRecordsController.getAll)
-router.get('/:id',vinylRecordsController.getById)
+router.post('/', vinylRecordsController.create)
+router.get('/', vinylRecordsController.getAll)
+router.get('/:id', vinylRecordsController.getById)
 
 module.exports = router
