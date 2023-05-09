fe66b74e016cbcd79156779ec46fe9115fb09825
bratanovv <bratanov.misha@mail.ru>
2023-05-09 19:41:39 +0300

    error tracking

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,12 +1,14 @@
 class UserController {
-    async registration(req,res){
+    async registration(req, res) {
 
     }
-    async login(req,res){
+
+    async login(req, res) {
 
     }
-    async check(req,res){
-    const {id} = req.query
+
+    async check(req, res) {
+        const {id} = req.query
         res.json(id)
     }
 
